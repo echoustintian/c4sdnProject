2e3c509c6fda740de1fc18dd2f42af292bb99028
gitguoxiangyu <3317755916@qq.com>
2023-05-20 21:46:58 +0800

    feat: add and use Pinia plugins

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 // 使用插件
 app.use(router)
+app.use(createPinia())
 
 app.mount('#app')
---
+++ src/stores/index.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+
+export const useMainStore = defineStore('Main',{
+  state: ()=>{
+    return {
+
+    }
+  },
+  // 类似computed ， 修饰一些值
+  getters: {
+
+  },
+  actions: {
+
+  }
+})
