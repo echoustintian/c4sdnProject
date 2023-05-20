7b4ce5d30a754fa4f04cf4cfeeb3e2bf6c3fe9ed
gitguoxiangyu <3317755916@qq.com>
2023-05-20 22:08:49 +0800

    feat: add element-plus

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 // 使用插件
+app.use(ElementPlus)
 app.use(router)
 app.use(createPinia())
 
