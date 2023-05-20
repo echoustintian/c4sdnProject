24b4ec5222cca7606d13ffd64f4a644b489a1b84
gitguoxiangyu <3317755916@qq.com>
2023-05-20 20:50:56 +0800

    project init

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 依赖预构建优化
+  optimizeDeps:{
+    exclude: []
+  },
+
+  resolve: {
+    // 路径别名
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+
+  build: {
+    // 仅供测试，便于定位生成环境问题,生产上线记得关闭
+    sourcemap: true,
+    minify: false,
+    // rollupOptions 配置
+    rollupOptions:{
+      output:{
+
+      }
+    }
+  },
+
+  
+
+  plugins: [vue()],
+})
