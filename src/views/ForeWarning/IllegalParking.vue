2cdca3052dc546f33c59fa8eafc8b33e28ca8375
YogMas <90102110+YogMas@users.noreply.github.com>
2023-06-13 17:33:14 +0800

    fix:修复报警中iframe url问题

--- src/views/ForeWarning/IllegalParking.vue
+++ src/views/ForeWarning/IllegalParking.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="Warning">
-        <iframe src="http://122.9.166.142:3000/d/xtkCtBkiz/yuan-qu-gao-jing-jian-kong?orgId=1"
-            onload="this.height=if_content.document.body.scrollHeight" frameborder="0"></iframe>
+        <div class="outer"></div>
+        <iframe src="http://122.9.166.142:3000/d/xtkCtBkiz/yuan-qu-gao-jing-jian-kong?orgId=1&kiosk" frameborder="0"
+            id="inneriframe"></iframe>
     </div>
 </template>
 
@@ -11,11 +12,16 @@
 
 <style scoped lang="less">
 .Warning {
+    position: relative;
     padding: 0;
     margin: 0;
+    overflow: hidden;
+    width: 100%;
     height: calc(100vh - 70px);
 
-    iframe {
+
+    #inneriframe {
+        position: absolute;
         width: 100%;
         height: 100%;
     }
